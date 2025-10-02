e9a4b6c4d900afebf99a82049f9d876930aaeaa5
flaka <flaka@SaraMarin>
2025-10-01 19:48:27 -0500

    parcial

--- JavaApplication120/src/javaapplication120/JavaApplication120.java
+++ JavaApplication120/src/javaapplication120/JavaApplication120.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication120;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -14,7 +16,27 @@ public class JavaApplication120 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner read=new Scanner(System.in);
+        Random rand  = new Random();
+        
+        int num = 0;
+        int sumaNum = 0;
+        
+        System.out.println("Digite un numero: ");
+        num = read.nextInt();
+         
+        int[] arreglo = new int[num];
+        
+        for (int i=0; i<num; i++){
+            arreglo[i] = 100 + rand.nextInt(99999 - 100 + 1);
+        }
+        
+        for (int i=0; i < num; i++) {
+            System.out.print(arreglo[i] + (i < num - 1 ? ", " : ""));
+        }
+        for (int i=0; i<arreglo.length; i++){
+        }
+
     }
     
 }
